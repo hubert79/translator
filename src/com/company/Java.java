69cbe9df7336d9e2b2720aab4773bf75f8b62ea5
hubert79 <makula.hubert@gmail.com>
2020-04-17 01:27:32 +0200

    add all functions

--- src/com/company/Java.java
+++ src/com/company/Java.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.io.IOException;
+import java.util.NoSuchElementException;
 import java.util.StringTokenizer;
 
 public class Java extends Language{
@@ -23,6 +24,7 @@ public class Java extends Language{
     public static final String CHAR = "char";
     public static final String BOOLEAN = "boolean";
     public static final String SEMI = ";";
+    public static final String INPUT = "input.readLine()";
 
     public Java(String toLang) throws IOException {
         super(toLang);
@@ -143,25 +145,271 @@ public class Java extends Language{
     @Override
     public void method(String str) throws IOException{
 
-        //System.out.println("test");
+        boolean isComment = false;
+        StringTokenizer st = new StringTokenizer(str);
+        String name = st.nextToken(); //"public"
+        name = st.nextToken(); //return type
+        name = st.nextToken();
+        String parameters = "";
+        String parameter;
+        do {
+            parameter = st.nextToken().replace("(", "");
+            if (parameter.equals(COMMENT)) {
+                isComment = true;
+                break;
+            } else if (!(parameter.equals(CHAR) || parameter.equals(INT) || parameter.equals(BOOLEAN)
+                    || parameter.equals(DOUBLE) || parameter.equals(STRING))) {
+                parameters+= " " + parameter;
+            }
+        } while (st.hasMoreTokens());
+        parameters = parameters.replace(")", "");
+        if (toLang.equals("Python")) {
+            System.out.println();
+            System.out.print("def " + name);
+            if (parameters.equals(" ")) {
+                System.out.print("(self):");
+            } else {
+                System.out.print("(self, " + parameters.replace(" ", "") + "):");
+            }
+        } else {
+            System.out.print("this." + name + " = function (" + parameters + ")");
+        }
+        if (isComment) {
+            String comment = "";
+            String nextField;
+            do {
+                nextField = st.nextToken();
+                comment+= " " + nextField;
+            } while (st.hasMoreTokens());
+            this.lineComment(comment);
+        }
     }
 
     @Override
     public void body(String str) throws IOException{
 
-        //System.out.println("test");
+        boolean isComment = false;
+        StringTokenizer st = new StringTokenizer(str);
+        String first = st.nextToken();
+        if (first.equals("System.out.println") || first.equals("return") || str.indexOf("this.") >= 0) {
+            String rest = "";
+            String next;
+            while (st.hasMoreTokens()) {
+                next = st.nextToken();
+                if (next.equals(COMMENT)) {
+                    isComment = true;
+                    break;
+                }
+                rest+= " " + next;
+            }
+            if (first.equals("System.out.println")) { // print line
+                if (toLang.equals("Python")) {
+                    System.out.print("print" + rest.replace(SEMI, ""));
+                } else {
+                    System.out.print("console.log" + rest);
+                }
+            } else if (first.equals("return")) { // return statement
+                if (toLang.equals("Python")) {
+                    System.out.print(first + rest.replace(SEMI, ""));
+                } else {
+                    System.out.print(first + rest);
+                }
+            } else { // method call
+                if (toLang.equals("Python")) {
+                    System.out.print(first.replace("this.", "self.") + rest.replace(SEMI, ""));
+                } else {
+                    System.out.print(first.replace("this.", "") + rest);
+                }
+            }
+            System.out.println();
+        }
+        else if (str.indexOf("=") > 0) { // variable declaration
+            String equals = st.nextToken(); //"="
+            String value;
+            if (!(equals.equals("="))) { // presence of a different variable type
+                first = equals;
+                value = st.nextToken();
+                value = st.nextToken();
+            } else {
+                value = st.nextToken();
+                if (str.indexOf(INPUT) >= 0) { // input statement
+                    String rest = "";
+                    String next;
+                    while (st.hasMoreTokens()) {
+                        next = st.nextToken();
+                        if (next.equals(COMMENT)) {
+                            isComment = true;
+                            break;
+                        }
+                        rest+= " " + next;
+                    }
+                    if (toLang.equals("Python")) {
+                        System.out.println("input(" + rest);
+                    } else {
+                        System.out.println("prompt(" + rest + ");");
+                    }
+                    System.out.println();
+                }
+            }
+            if (toLang.equals("Python")) {
+                System.out.print("self." + first + " = " + value.replace(";", ""));
+            } else {
+                System.out.print("this." + first + " = " + value);
+            }
+            System.out.println();
+        } else if (first.equals("private")) {
+            // write nothing and do not add a new line
+        } else {
+            String rest = "";
+            String next = st.nextToken();
+            while (st.hasMoreTokens()) {
+                if (next.equals(COMMENT)) {
+                    isComment = true;
+                    break;
+                }
+                rest+= next;
+                next = st.nextToken();
+            }
+            if (toLang.equals("Python")) {
+                System.out.print(rest.replace(";", ""));
+            } else {
+                System.out.print(rest); //write whatever is contained.
+            }
+            System.out.println();
+        }
+        try {
+            if (isComment || st.nextToken().equals(COMMENT)) {
+                String comment = "";
+                String next;
+                do {
+                    next = st.nextToken();
+                    if (!(next.equals(COMMENT))) {
+                        comment+= " " + next;
+                    }
+                } while (st.hasMoreTokens());
+                this.lineComment(comment);
+            }
+        }
+        catch (NoSuchElementException e) {
+            // no comment
+        }
     }
 
     @Override
     public void forLoop(String str) throws IOException{
 
-        //System.out.println("test");
+        StringTokenizer st = new StringTokenizer(str);
+        try {
+            String tFor = st.nextToken(); // "for"
+            String name = st.nextToken(); // "(int"
+            name = st.nextToken(); //"x"
+            String determine = st.nextToken(); // ":" or "="
+            if (determine.equals(":")) { // check for a for-each loop.
+                String object = st.nextToken();
+                if (toLang.equals("Python")) {
+                    object = object.replace(")", "");
+                    System.out.print(tFor + " " + name + " in " + object + ":");
+                } else {
+                    System.out.print(tFor + "(var " + name + " in " + object);
+                }
+            } else { // regular for loop.
+                String start = st.nextToken(); // starting integer
+                name = st.nextToken(); // "x"
+                String operator = st.nextToken(); // "<"
+                String stop = st.nextToken(); // ending integer
+                String step = st.nextToken();
+                if (step.indexOf("++") > 0 || step.indexOf("--") > 0) {
+                    if (toLang.equals("Python")) {
+                        start = start.replace(SEMI, "");
+                        stop = stop.replace(SEMI, "");
+                        System.out.print(tFor + " " + name + " in range(" + start + ", " + stop + "):");
+                    } else {
+                        System.out.print(tFor + " (var " + name + " " + determine + " " + start + " "
+                                + name + " " + operator + " " + stop + " " + step);
+                    }
+                } else {
+                    if (toLang.equals("Python")) {
+                        String increment = st.nextToken();
+                        increment = st.nextToken();
+                        increment = st.nextToken();
+                        increment = st.nextToken();
+                        start = start.replace(SEMI, "");
+                        stop = stop.replace(SEMI, "");
+                        System.out.print(tFor + " " + name + " in range(" + start + ", " + stop + ", "
+                                + increment + ":");
+                    } else {
+                        String increment = st.nextToken();
+                        increment = st.nextToken();
+                        increment = st.nextToken();
+                        String arithmetic = increment;
+                        increment = st.nextToken();
+                        System.out.print(tFor + " (var " + name + " " + determine + " " + start + " "
+                                + name + " " + operator + " " + stop + " " + name + " " + determine
+                                + " " + name + " " + arithmetic + " " + increment);
+                    }
+                }
+            }
+        }
+        catch (NoSuchElementException e) {
+            System.out.print("Error in 'for'-loop.");
+        }
+        try {
+            if (st.nextToken().equals(COMMENT)) {
+                String comment = "";
+                while(st.hasMoreTokens()) {
+                    comment+= " " + st.nextToken();
+                }
+                this.lineComment(comment);
+            }
+        }
+        catch (NoSuchElementException e) {
+            // No comment.
+        }
     }
 
     @Override
     public void ifWhileStatement(String str) throws IOException{
 
-        //System.out.println("test");
+        boolean isComment = false;
+        StringTokenizer st = new StringTokenizer(str);
+        String first = st.nextToken();
+        if (first.equals(ELSE)) {
+            System.out.print(ELSE);
+        } else {
+            System.out.print(first + " "); // "if" or "while"
+            String syntax;
+            do {
+                syntax = st.nextToken();
+                if (syntax.equals(COMMENT)) {
+                    isComment = true;
+                    break;
+                }
+                if (toLang.equals("Python")) {
+                    if (syntax.equals("||")) {
+                        System.out.print(" or ");
+                    } else if (syntax.equals("&&")) {
+                        System.out.print(" and ");
+                    } else if (syntax.equals("!")) {
+                        System.out.print(" not ");
+                    } else {
+                        System.out.print(syntax.replace(")", "").replace("(", "")); // Write whatever is being
+                        // compared or inequalities.
+                    }
+                } else {
+                    System.out.print(" " + syntax + " "); // Same syntax as Java.
+                }
+            } while (st.hasMoreTokens());
+            if (toLang.equals("Python")) {
+                System.out.print(":");
+            }
+        }
+        if (isComment) {
+            String comment = "";
+            while (st.hasMoreTokens()) {
+                comment+= " " + st.nextToken();
+            }
+            this.lineComment(comment);
+        }
     }
 
     @Override
