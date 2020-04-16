e87193a93df15d283229c1a9a961344d84e4f7a6
hubert79 <makula.hubert@gmail.com>
2020-04-17 00:14:15 +0200

    Coment

--- src/com/company/Java.java
+++ src/com/company/Java.java
@@ -4,6 +4,25 @@ import java.io.IOException;
 
 public class Java extends Language{
 
+    public static final String COMMENT = "//";
+    //public static final String JAVADOC1 = "/**";
+    //public static final String JAVADOC2 = "*";
+    //public static final String JAVADOC3 = "*/";
+    //public static final String IF = "if";
+    //public static final String ELSE = "else";
+    //public static final String WHILE = "while";
+    //public static final String FOR = "for";
+    //public static final String I_STRUCTURE = "{";
+    //public static final String F_STRUCTURE = "}";
+    //public static final String PUBLIC = "public";
+    //public static final String VOID = "void";
+    //public static final String INT = "int";
+    //public static final String STRING = "String";
+    //public static final String DOUBLE = "double";
+    //public static final String CHAR = "char";
+    //public static final String BOOLEAN = "boolean";
+    //public static final String SEMI = ";";
+
     public Java(String toLang) throws IOException {
         super(toLang);
     }
@@ -17,8 +36,8 @@ public class Java extends Language{
         for (String a : syntax){
 
             //System.out.print(a);
-            if(a.equals("public")){
-                System.out.print("xxx");
+            if(a.equals(COMMENT)){
+                lineComment(a);
             }
             else {
                 System.out.print(a);
@@ -26,5 +45,47 @@ public class Java extends Language{
             System.out.print(" ");
         }
 
+    }
+
+    @Override
+    public void constructor(String str) throws IOException{
+
+        System.out.println("test");
+    }
+
+    @Override
+    public void method(String str) throws IOException{
+
+        System.out.println("test");
+    }
+
+    @Override
+    public void body(String str) throws IOException{
+
+        System.out.println("test");
+    }
+
+    @Override
+    public void forLoop(String str) throws IOException{
+
+        System.out.println("test");
+    }
+
+    @Override
+    public void ifWhileStatement(String str) throws IOException{
+
+        System.out.println("test");
+    }
+
+    @Override
+    public void lineComment(String str) throws IOException{
+
+        System.out.print("#" + str.replace(COMMENT, ""));
+    }
+
+    @Override
+    public void longComment(String str) throws IOException{
+
+        System.out.println("test");
     }
 }
--- src/com/company/Language.java
+++ src/com/company/Language.java
@@ -7,35 +7,41 @@ import java.io.InputStreamReader;
 
 public abstract class Language {
 
-public String toLang;
+        public String toLang;
 
-public Language(String toLang) throws IOException {}
+        public Language(String toLang) throws IOException {}
 
-public void read() throws  IOException{
+        public void read() throws  IOException{
 
-        InputStreamReader reader = new InputStreamReader (System.in);
-        BufferedReader input = new BufferedReader (reader);
-        System.out.println("Enter the name of the text file imported: ");
-        FileReader readFile = new FileReader(input.readLine());
-        BufferedReader inFile = new BufferedReader(readFile);
-        System.out.println();
-        System.out.println("Reading file...");
-        String inputString = inFile.readLine();
+                InputStreamReader reader = new InputStreamReader (System.in);
+                BufferedReader input = new BufferedReader (reader);
+                System.out.println("Enter the name of the text file imported: ");
+                FileReader readFile = new FileReader(input.readLine());
+                BufferedReader inFile = new BufferedReader(readFile);
+                System.out.println();
+                System.out.println("Reading file...");
+                String inputString = inFile.readLine();
 
-        while (inputString != null) {
+                while (inputString != null) {
 
-        //System.out.println(inputString);
-        System.out.println();
-        this.translate(inputString);
-        inputString = inFile.readLine();
+                        //System.out.println(inputString);
+                        System.out.println();
+                        this.translate(inputString);
+                        inputString = inFile.readLine();
+                }
+
+                System.out.println();
+                System.out.println("Reading completed.");
+
+                inFile.close();
         }
 
-        System.out.println();
-        System.out.println("Reading completed.");
-
-        inFile.close();
-        }
-
-    public abstract void translate(String str) throws IOException;
-
+        public abstract void translate(String str) throws IOException;
+        public abstract void constructor(String str) throws IOException;
+        public abstract void method(String str) throws IOException;
+        public abstract void body(String str) throws IOException;
+        public abstract void forLoop(String str) throws IOException;
+        public abstract void ifWhileStatement(String str) throws IOException;
+        public abstract void lineComment(String str) throws IOException;
+        public abstract void longComment(String str) throws IOException;
 }
