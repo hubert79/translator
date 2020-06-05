b436c79b75cd5bdffee669fefb73be9df1115a02
hubertmakula <hubertmakula@MacBook-Air-hubertmakula.local>
2020-06-05 23:23:29 +0200

    generated antlr files

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 public class Main {
 
@@ -21,6 +24,8 @@ public class Main {
         }
     }
     public static void main(String[] args) throws IOException{
-        answer();
+        // answer();
+
+
     }
 }
