663dff00ac530a228b519e4f27bda59d948a0055
hubertmakula <hubertmakula@MacBook-Air-hubertmakula.local>
2020-06-07 13:16:40 +0200

    my grammar

---
+++ gen/gramatykaListener.java
@@ -0,0 +1,279 @@
+// Generated from gramatyka.g4 by ANTLR 4.8
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link gramatykaParser}.
+ */
+public interface gramatykaListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#kompilacja}.
+	 * @param ctx the parse tree
+	 */
+	void enterKompilacja(gramatykaParser.KompilacjaContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#kompilacja}.
+	 * @param ctx the parse tree
+	 */
+	void exitKompilacja(gramatykaParser.KompilacjaContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#deklaracjiaPaczki}.
+	 * @param ctx the parse tree
+	 */
+	void enterDeklaracjiaPaczki(gramatykaParser.DeklaracjiaPaczkiContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#deklaracjiaPaczki}.
+	 * @param ctx the parse tree
+	 */
+	void exitDeklaracjiaPaczki(gramatykaParser.DeklaracjiaPaczkiContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#deklaracjaImportu}.
+	 * @param ctx the parse tree
+	 */
+	void enterDeklaracjaImportu(gramatykaParser.DeklaracjaImportuContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#deklaracjaImportu}.
+	 * @param ctx the parse tree
+	 */
+	void exitDeklaracjaImportu(gramatykaParser.DeklaracjaImportuContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#typDeklaracji}.
+	 * @param ctx the parse tree
+	 */
+	void enterTypDeklaracji(gramatykaParser.TypDeklaracjiContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#typDeklaracji}.
+	 * @param ctx the parse tree
+	 */
+	void exitTypDeklaracji(gramatykaParser.TypDeklaracjiContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#deklaracjaKlasy}.
+	 * @param ctx the parse tree
+	 */
+	void enterDeklaracjaKlasy(gramatykaParser.DeklaracjaKlasyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#deklaracjaKlasy}.
+	 * @param ctx the parse tree
+	 */
+	void exitDeklaracjaKlasy(gramatykaParser.DeklaracjaKlasyContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#cialoKlasy}.
+	 * @param ctx the parse tree
+	 */
+	void enterCialoKlasy(gramatykaParser.CialoKlasyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#cialoKlasy}.
+	 * @param ctx the parse tree
+	 */
+	void exitCialoKlasy(gramatykaParser.CialoKlasyContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#wnetrze}.
+	 * @param ctx the parse tree
+	 */
+	void enterWnetrze(gramatykaParser.WnetrzeContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#wnetrze}.
+	 * @param ctx the parse tree
+	 */
+	void exitWnetrze(gramatykaParser.WnetrzeContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#deklaracjaMetody}.
+	 * @param ctx the parse tree
+	 */
+	void enterDeklaracjaMetody(gramatykaParser.DeklaracjaMetodyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#deklaracjaMetody}.
+	 * @param ctx the parse tree
+	 */
+	void exitDeklaracjaMetody(gramatykaParser.DeklaracjaMetodyContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#deklaracjaSkladowych}.
+	 * @param ctx the parse tree
+	 */
+	void enterDeklaracjaSkladowych(gramatykaParser.DeklaracjaSkladowychContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#deklaracjaSkladowych}.
+	 * @param ctx the parse tree
+	 */
+	void exitDeklaracjaSkladowych(gramatykaParser.DeklaracjaSkladowychContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#modyfikator}.
+	 * @param ctx the parse tree
+	 */
+	void enterModyfikator(gramatykaParser.ModyfikatorContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#modyfikator}.
+	 * @param ctx the parse tree
+	 */
+	void exitModyfikator(gramatykaParser.ModyfikatorContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#stala}.
+	 * @param ctx the parse tree
+	 */
+	void enterStala(gramatykaParser.StalaContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#stala}.
+	 * @param ctx the parse tree
+	 */
+	void exitStala(gramatykaParser.StalaContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#parametrMetody}.
+	 * @param ctx the parse tree
+	 */
+	void enterParametrMetody(gramatykaParser.ParametrMetodyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#parametrMetody}.
+	 * @param ctx the parse tree
+	 */
+	void exitParametrMetody(gramatykaParser.ParametrMetodyContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#deklaracjaParametrowMetod}.
+	 * @param ctx the parse tree
+	 */
+	void enterDeklaracjaParametrowMetod(gramatykaParser.DeklaracjaParametrowMetodContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#deklaracjaParametrowMetod}.
+	 * @param ctx the parse tree
+	 */
+	void exitDeklaracjaParametrowMetod(gramatykaParser.DeklaracjaParametrowMetodContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#cialoMetody}.
+	 * @param ctx the parse tree
+	 */
+	void enterCialoMetody(gramatykaParser.CialoMetodyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#cialoMetody}.
+	 * @param ctx the parse tree
+	 */
+	void exitCialoMetody(gramatykaParser.CialoMetodyContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#deklaracjaZmiennej}.
+	 * @param ctx the parse tree
+	 */
+	void enterDeklaracjaZmiennej(gramatykaParser.DeklaracjaZmiennejContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#deklaracjaZmiennej}.
+	 * @param ctx the parse tree
+	 */
+	void exitDeklaracjaZmiennej(gramatykaParser.DeklaracjaZmiennejContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#instrukcja}.
+	 * @param ctx the parse tree
+	 */
+	void enterInstrukcja(gramatykaParser.InstrukcjaContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#instrukcja}.
+	 * @param ctx the parse tree
+	 */
+	void exitInstrukcja(gramatykaParser.InstrukcjaContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#inicjalizaacjaZmiennej}.
+	 * @param ctx the parse tree
+	 */
+	void enterInicjalizaacjaZmiennej(gramatykaParser.InicjalizaacjaZmiennejContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#inicjalizaacjaZmiennej}.
+	 * @param ctx the parse tree
+	 */
+	void exitInicjalizaacjaZmiennej(gramatykaParser.InicjalizaacjaZmiennejContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#typ}.
+	 * @param ctx the parse tree
+	 */
+	void enterTyp(gramatykaParser.TypContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#typ}.
+	 * @param ctx the parse tree
+	 */
+	void exitTyp(gramatykaParser.TypContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#komunikat}.
+	 * @param ctx the parse tree
+	 */
+	void enterKomunikat(gramatykaParser.KomunikatContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#komunikat}.
+	 * @param ctx the parse tree
+	 */
+	void exitKomunikat(gramatykaParser.KomunikatContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#wyrazenie}.
+	 * @param ctx the parse tree
+	 */
+	void enterWyrazenie(gramatykaParser.WyrazenieContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#wyrazenie}.
+	 * @param ctx the parse tree
+	 */
+	void exitWyrazenie(gramatykaParser.WyrazenieContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#dubwyrazenie}.
+	 * @param ctx the parse tree
+	 */
+	void enterDubwyrazenie(gramatykaParser.DubwyrazenieContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#dubwyrazenie}.
+	 * @param ctx the parse tree
+	 */
+	void exitDubwyrazenie(gramatykaParser.DubwyrazenieContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#literal}.
+	 * @param ctx the parse tree
+	 */
+	void enterLiteral(gramatykaParser.LiteralContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#literal}.
+	 * @param ctx the parse tree
+	 */
+	void exitLiteral(gramatykaParser.LiteralContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#typPrymitywny}.
+	 * @param ctx the parse tree
+	 */
+	void enterTypPrymitywny(gramatykaParser.TypPrymitywnyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#typPrymitywny}.
+	 * @param ctx the parse tree
+	 */
+	void exitTypPrymitywny(gramatykaParser.TypPrymitywnyContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#nazwaKlasy}.
+	 * @param ctx the parse tree
+	 */
+	void enterNazwaKlasy(gramatykaParser.NazwaKlasyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#nazwaKlasy}.
+	 * @param ctx the parse tree
+	 */
+	void exitNazwaKlasy(gramatykaParser.NazwaKlasyContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#nazwaMetody}.
+	 * @param ctx the parse tree
+	 */
+	void enterNazwaMetody(gramatykaParser.NazwaMetodyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#nazwaMetody}.
+	 * @param ctx the parse tree
+	 */
+	void exitNazwaMetody(gramatykaParser.NazwaMetodyContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#nazwa}.
+	 * @param ctx the parse tree
+	 */
+	void enterNazwa(gramatykaParser.NazwaContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#nazwa}.
+	 * @param ctx the parse tree
+	 */
+	void exitNazwa(gramatykaParser.NazwaContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gramatykaParser#nazwaZmiennej}.
+	 * @param ctx the parse tree
+	 */
+	void enterNazwaZmiennej(gramatykaParser.NazwaZmiennejContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gramatykaParser#nazwaZmiennej}.
+	 * @param ctx the parse tree
+	 */
+	void exitNazwaZmiennej(gramatykaParser.NazwaZmiennejContext ctx);
+}
